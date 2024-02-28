a418dac1a365be7d322302eecd544f0b2a45a10e
tanrongjun <tanrj@meix.com>
2024-02-28 10:45:27 +0800

    feat:新增错误边界代码

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ChildCom1 from "./components/ChildCom1";
+import ChildCom2 from "./components/ChildCom2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      padding: "10px",
+      border: "1px solid"
+    }}>
+      App组件
+      <ChildCom1 />
+      <ChildCom2 />
     </div>
   );
 }
---
+++ src/components/ChildCom4.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function getData() {
+  // return ["HTML", "CSS", "JS"]
+}
+
+function ChildCom3() {
+  const arr = getData();
+
+  const lis = arr.map((it, index) => <li key={index}>{it}</li>);
+
+  return (
+    <ul
+      style={{
+        border: "1px solid",
+      }}
+    >
+      {lis}
+    </ul>
+  );
+}
+
+export default ChildCom3;
